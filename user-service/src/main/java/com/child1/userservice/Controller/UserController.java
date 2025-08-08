33c832af4dd76a5ee62bd298c4a4f399871df382
Mohammad zaid Shaikh <109480030+Zaidshaikh2811@users.noreply.github.com>
2025-08-08 16:18:42 +0530

    remove merge conflict markers from docker-compose.yml

---
+++ user-service/src/main/java/com/child1/userservice/Controller/UserController.java
@@ -0,0 +1,15 @@
+package com.child1.userservice.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserController {
+
+    @GetMapping()
+    public String getUser() {
+        return "Hello from User Service";
+    }
+}
