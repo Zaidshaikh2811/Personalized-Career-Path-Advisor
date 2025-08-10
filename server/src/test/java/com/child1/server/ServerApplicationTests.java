8663e6f1c39c5cce08d899079907e3163e937f00
Mohammad zaid Shaikh <109480030+Zaidshaikh2811@users.noreply.github.com>
2025-08-10 10:07:44 +0530

    add Maven wrapper and configuration files for project setup

---
+++ server/src/test/java/com/child1/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.child1.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
